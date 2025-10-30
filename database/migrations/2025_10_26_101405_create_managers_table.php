53bc8849d03a5a2a2c98199cb0bf20fdc1fe57dc
isikjon <isikjandev@gmail.com>
2025-10-30 22:16:56 +0500

    101% 30.10.2025

--- database/migrations/2025_10_26_101405_create_managers_table.php
+++ database/migrations/2025_10_26_101405_create_managers_table.php
@@ -8,17 +8,19 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('managers', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('phone');
-            $table->string('whatsapp')->nullable();
-            $table->string('instagram')->nullable();
-            $table->string('telegram')->nullable();
-            $table->boolean('is_active')->default(true);
-            $table->integer('order')->default(0);
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('managers')) {
+            Schema::create('managers', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('phone');
+                $table->string('whatsapp')->nullable();
+                $table->string('instagram')->nullable();
+                $table->string('telegram')->nullable();
+                $table->boolean('is_active')->default(true);
+                $table->integer('order')->default(0);
+                $table->timestamps();
+            });
+        }
     }
 
     public function down(): void
